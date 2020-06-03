d409b11894bd439c92ab81893459404539d95e15
Matthew Rinne <mdrinne@att.net>
2020-06-03 00:00:14 -0500

    starting to learn to develop server code

---
+++ upTheRiver/Models/Player.swift
@@ -0,0 +1,21 @@
+//
+//  Player.swift
+//  upTheRiver
+//
+//  Created by Matthew Rinne on 6/1/20.
+//  Copyright © 2020 Matthew Rinne. All rights reserved.
+//
+
+import Foundation
+
+//class Player {
+//    let fullName: String
+//    let username: String
+//    let uid: String
+//    let nickname: String
+//    let cards: [String]
+//    
+//    init(fullName: String) {
+//        <#statements#>
+//    }
+//}
---
+++ upTheRiver/Models/Table.swift
@@ -0,0 +1,16 @@
+//
+//  Table.swift
+//  upTheRiver
+//
+//  Created by Matthew Rinne on 6/1/20.
+//  Copyright © 2020 Matthew Rinne. All rights reserved.
+//
+
+import Foundation
+
+//class Table {
+//    let owner: String
+//    let tableName: String
+//    let playerCount: Int
+//    let players: [Player]
+//}
